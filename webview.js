c756e3c94185e9fed2f43be183632b751111e2e0
Kan Fushihara <kan.fushihara@gmail.com>
2016-09-06 14:38:27 +0900

    未読リプライ数の取得ロジックを修正した

--- webview.js
+++ webview.js
@@ -2,10 +2,10 @@ module.exports = (Franz, options) => {
     let first_stmt = $($('#statements .statement')[0]).attr('class');
 
     function getMessages() {
-        const res = $('new-res-notice-text').text().match(/新着レス(\d+)?が個あります/);
+        const res = $('#new-res-notice-text').text().match(/新着レスが(\d+)?個あります/);
         var reply = 0;
         if (res) {
-            reply = res[0];
+            reply = res[1];
         }
 
         Franz.setBadge(reply);
