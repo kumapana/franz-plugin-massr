11b3e5c0c3279f89ab0a20aee84d8ff100a46224
Kan Fushihara <kan.fushihara@gmail.com>
2016-09-15 16:15:41 +0900

    自動更新時に未読チェックするよう修正
    - スクロールをhookしてリセット

--- webview.js
+++ webview.js
@@ -1,5 +1,10 @@
 module.exports = (Franz, options) => {
     let first_stmt = $($('#statements .statement')[0]).attr('class');
+    Franz.latestStatement = $('div#statements .statement')[0].className;
+
+    $(window).bind('scroll', (ev) => {
+        Franz.latestStatement = $('div#statements .statement')[0].className;
+    });
 
     function getMessages() {
         const res = $('#new-res-notice-text').text().match(/新着レスが(\d+)?個あります/);
@@ -7,8 +12,13 @@ module.exports = (Franz, options) => {
         if (res) {
             reply = res[1];
         }
+        var unread = 0;
+        const ls = $('div#statements .statement')[0].className;
+        if (ls != Franz.latestStatement) {
+            unread = 1;
+        }
 
-        Franz.setBadge(reply);
+        Franz.setBadge(reply, unread);
     }
 
     Franz.loop(getMessages);
